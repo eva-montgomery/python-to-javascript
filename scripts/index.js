913263d1b94e665cc5f9168abe117c45938f40ec
eva montgomery <eva.m.montgomery@gmail.com>
2019-12-02 14:19:10 -0500

    adds more looping examples

--- scripts/index.js
+++ scripts/index.js
@@ -123,7 +123,138 @@ for (let item of turkeyDinner) {
     // same as itemCount += 1
 }
 
-
-
-
 // 3. (for tomorrow) .forEach and friends
+
+// Converting Numbers to/from Strings
+// a_num = "5"
+// no_really_number = int(num)
+const aNum = "5";
+// const reallyANumber = Number(aNum);
+const reallyANumber = parseInt(aNum, 10);
+// Passing parseInt a dicimal number results in the decimal portion being throwna away:
+// parseInt("3.14", 10) --> 3
+
+const reallyAFloat = parseFloat("3.14");
+
+// the_original_number = str(no_really_a_number)
+const theStringFive = String(reallyANumber);
+const theString314 = String(reallyAFloat);
+
+// Comparison
+
+// In python, this is false:
+// "5" == 5
+
+// In JavaScript, the == operator 
+// AUTOMATICALLY converts types.
+
+// In JavaScript, use the "threequal" ===
+"5" === 5
+// `===` is the strict equality operator
+// it does a double comparison.
+// 1. It compares the types
+// 2. then. it compares the values
+
+
+// These are the same in python and in JavaScript
+5 < 6 // true
+6 < 6 // false
+6 <= 6 // true
+7 > 6 // true
+7 > 7 // false
+7 >= 7 // true
+
+// the following is not a thing in JavaScript:
+// "7" >== 7
+// you get an error!‚
+
+// and/or equivalents
+const petAge = 9;
+
+//python
+// petAge < 10 and petAge > 5
+
+// JavaScript
+petAge < 10 && petAge > 5;
+
+// python's or
+// petAge < 10 or petAge > 5
+
+// Javascript
+petAge < 10 || petAge > 5
+
+// too much
+petAge < 10 && petAge > 5 || petAge === 8;
+const isYoungEnough = petAge < 10;
+const isOldEnough = petAge > 5;
+const isJustRight = petAge === 8;
+
+isYoungEnough && isOldEnough || isJustRight;
+
+// if () {}
+
+// if/else
+if (isYoungEnough && isOldEnough) {
+    if (isJustRight) {
+        console.log(`You are exactly the right age.`)
+         } else {
+            console.log(`You're cool anyway.`)
+}
+}
+
+// if (isYoungEnough && isOldEnough) {
+//     if (isJustRight) {
+//         console.log(`You are exactly the right age.`)
+//     } else if(isMoreJustRight) {
+//         console.log(`Wow. That's the best`)
+//          } else {
+//             console.log(`You're cool anyway.`)
+// }
+// }
+
+// 1. Create an array of numbers
+// 2. Loop through the array.
+// 3. Print out only the even numbers.
+
+// in python:
+// num = 8123456
+// num % 2 == 0
+
+// in JavaScript:
+// const num = 8123456
+// (num % 2) === 0;
+
+const someNumbers = [25, 3, 8, 54, 8, 2, 5, 7, 12, 66];
+// for () {}
+// the `in` keyword would give you the indexes-
+// for other kinds of collections, the `in` keyword can give you "metadata"
+for (let number of someNumbers) {
+    if (number % 2 === 0) {
+        console.log(`Here is a number: ${number}`);
+        // console.log(number);
+    }
+}
+
+// I want the even numbers in their own array.
+// - Where are the numbers now? In an array
+// - Where do I want to put them in? A new array
+const evensOnly = [];
+// How do I put them in the new array?
+
+for (let number of someNumbers) {
+    if (number % 2 === 0) {
+        // in python, it's evensOnly.append(number)
+        evensOnly.push(number);
+    }
+}
+console.log(evensOnly);
+
+// JavaScript doesn't care if you mix and match.
+// But, there's not a good use for a mixed up array.
+const aBunchOfStuff = ["a", 1, "hello", 300];
+for (let thing of aBunchOfStuff) {
+    console.log(thing);
+}
+
+// Maybe
+// Dictionaries
